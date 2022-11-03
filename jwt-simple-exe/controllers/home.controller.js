c6cbb4654531fbe232f64fc39081380b391c9bff
Who <blacklistuser420@gmail.com>
2022-11-03 10:35:36 +0000

    jwt refresh token start

--- jwt-simple-exe/controllers/home.controller.js
+++ jwt-simple-exe/controllers/home.controller.js
@@ -1,11 +1,8 @@
-const home = {
-    auth:'success',
-    authstyle:'style=background-color:var(--success)',
-    name:'Paing',
-    content:"successfully",
-}
 function getHome(req,res){
-    res.render('home');
+    res.status(200).json({
+        success:"Welcome! You'r successfully Login...!"
+    })
+
 };
 
 module.exports = {
